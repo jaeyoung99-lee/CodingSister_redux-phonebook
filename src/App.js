f04a12be54cc9a17dd0c15ff850685906a7752c2
jaeyoung99-lee <leejy0715@naver.com>
2024-08-26 01:27:02 +0900

    left UI design

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import { Col, Container, Row } from "react-bootstrap";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ContactForm from "./component/ContactForm";
 
 // 1. 왼쪽에는 연락처 등록하는 폼이, 오른쪽에는 연락처 리스트와 search 창이 있다
 // 2. 리스트에 유저 이름과 전화번호를 추가할 수 있다
@@ -6,7 +9,19 @@ import "./App.css";
 // 4. 사용자가 유저를 이름 검색으로 찾을 수 있다
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="title">연락처</h1>
+      <Container>
+        <Row>
+          <Col>
+            <ContactForm />
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
